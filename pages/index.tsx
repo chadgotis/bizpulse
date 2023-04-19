ade4d2e3eebb457e8095f86c4b6659a2d16ab274
Chad <Chadric.gotis@abcopro.com.au>
2023-04-19 15:42:07 +0800

    build: added cross-env and debugging build scripts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
-import Image from 'next/image';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <main>Next JS Template</main>;
+  return <main className="text-red-300">Next JS Template</main>;
 }
