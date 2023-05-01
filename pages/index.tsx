1df50e3e049025b1d002a032b5f0f744ea979629
Chad Gotis <Chad.mgotis@gmail.com>
2023-05-01 20:52:21 +0800

    fix: test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,3 @@
-// import { Inter } from 'next/font/google';
-
-// const inter = Inter({ subsets: ['latin'] });
-
 export default function Home() {
-  return <main className="text-red-300">Next JS Template</main>;
+  return <main className="">BizPulse</main>;
 }
